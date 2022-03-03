ca9cf92a634a99d6566dd33327ed72fff07c9f17
wangximing <sighwang@outlook.com>
2022-03-03 13:34:40 +0800

    test: start mock rpc

--- packages/ckb-indexer/tests/start_mock_rpc.ts
+++ packages/ckb-indexer/tests/start_mock_rpc.ts
@@ -1,14 +1,14 @@
-// import { createCKBMockRPC, mockData } from "@ckb-lumos/testkit";
-// import fs from "fs";
-// import path from "path";
+import { createCKBMockRPC, mockData } from "@ckb-lumos/testkit";
+import fs from "fs";
+import path from "path";
 console.log("in start mock server");
-// const server = createCKBMockRPC({
-//   blocks: JSON.parse(
-//     fs.readFileSync(path.join(__dirname, "./blocks_data.json")).toString()
-//   ),
-//   localNode: mockData.localNode(),
-// });
+const server = createCKBMockRPC({
+  blocks: JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./blocks_data.json")).toString()
+  ),
+  localNode: mockData.localNode(),
+});
 
-// server.listen(8118, function () {
-//   console.log("🚀 server listen to 8118");
-// });
+server.listen(8118, function () {
+  console.log("🚀 server listen to 8118");
+});
